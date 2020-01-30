f08bba3d3886b3bb5e86f77df65154f15b762a71
Federico <federico.caiello@gmail.com>
2020-01-30 18:53:35 +0100

    ajax con esito completato

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Griglia 6x6, ad ogni click sul quadrato parte una richiesta AJAX che prende un numero random da 1 a 9.
+// Se è <= 5 il quadrato diventa giallo,
+// se è > di 5 il quadrato diventa verde.
+// Il numero ottenuto appare al centro del quadrato.
+$(document).ready(function(){
+  $('.quadrato').click(function(){
+    var esito = $(this);
+    $.ajax(
+      {
+        'url' : "https://ﬂynn.boolean.careers/exercises/api/random/int",
+        'method' : "GET",
+        'success' : function (response) {
+          var numero = response.response;
+          esito.text(numero);
+          if (response.response <= 5) {
+            esito.addClass('colore_giallo');
+          }
+          else {
+            esito.addClass('colore_verde');
+          }
+        }
+      }
+    )
+  });
+});
